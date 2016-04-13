33fd610da6cc8cc5503eb2714697f9144198f859
qMcC <cooperjamesmccune@ymail.com>
2016-04-13 17:13:46 -0400

    Added icons, improved UI and started on timer method

--- pomodorotimer/pomodorotimer/Form1.cs
+++ pomodorotimer/pomodorotimer/Form1.cs
@@ -12,9 +12,35 @@ namespace pomodorotimer
 {
     public partial class Form1 : Form
     {
+        // button icons
+        Bitmap _buttonPlay = pomodorotimer.Properties.Resources.ButtonPlay;
+        Bitmap _buttonPause = pomodorotimer.Properties.Resources.ButtonPause;
+
+        // timer values
+        int genericTimeS = 300; // 300 s = 5 minutes
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == _buttonPause)
+            {
+                pictureBox1.Image = _buttonPlay;
+            }
+            else
+            {
+                pictureBox1.Image = _buttonPause;
+            }
+        }
+
+        private void timerDisplay_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
